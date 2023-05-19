170b086b6e98012395df0201d4b44d473017bddd
HumanClone <Pranhav1507@gmail.com>
2023-05-19 13:24:59 +0200

    Completed scaffolding and seession usage and connection string secured

---
+++ FarmerTracker/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FarmerTracker.Models
+{
+    public partial class Product
+    {
+        public int ProductId { get; set; }
+        public string Pname { get; set; }
+        public string Ptype { get; set; }
+        public DateTime? Pdate { get; set; }
+        public int? UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ FarmerTracker/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FarmerTracker.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int UserId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool? Farmer { get; set; }
+        public string UserPassword { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
